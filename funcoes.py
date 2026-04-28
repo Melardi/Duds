7719711f1af2f91ee0dcf3a9fcaa9cc9ed4aab5f
Eduardo Melardi <eduardomelardi@gmail.com>
2026-04-28 13:47:03 -0300

    ex 2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,23 @@ def rolar_dados(n):
         x = random.randint(1, 6)
         lista_final.append(x)
     return lista_final 
+
+
+
+
+
+
+def guardar_dado(lista_dados_rolados, lista_dados_guardados, indice_dado_para_guardar):
+    
+    valor_dado_para_guardar = lista_dados_rolados[indice_dado_para_guardar]
+
+    nova_lista_dados_rolados = []
+    for i in range(len(lista_dados_rolados)):
+        if i != indice_dado_para_guardar:
+            nova_lista_dados_rolados.append(lista_dados_rolados[i])
+    
+    lista_dados_guardados.append(valor_dado_para_guardar)
+
+    lista_resultado = [nova_lista_dados_rolados, lista_dados_guardados]
+
+    return lista_resultado
